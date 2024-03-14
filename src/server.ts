4e945863ec4a5752a819a75a05c8363fdb0a8add
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2024-03-14 13:17:32 -0300

    Ajustes para deploy

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import app from './app';
 
-const server = app.listen(3000, () => { 
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => { 
     // eslint-disable-next-line no-console
     console.log('Server is running'); 
 });
